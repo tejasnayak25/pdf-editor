0d0fac73c14b2ead74f3487b04f36869b9fadf55
tejasnayak25 <arpithanayak900@gmail.com>
2025-12-26 07:11:01 +0530

    fix: update file upload logic to use file directly instead of buffer

--- backend/storage.js
+++ backend/storage.js
@@ -5,7 +5,7 @@ class Storage {
     }
 
     async uploadFile(name, file) {
-        const fileData = await put(name, file.buffer, {
+        const fileData = await put(name, file, {
             access: "public"
         });
         return fileData.url;

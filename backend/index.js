5d96684b0934cffddddcf2b3f16f109d84cc162d
tejasnayak25 <arpithanayak900@gmail.com>
2025-12-27 11:14:42 +0530

    feat: Update file upload logic to store and return file URL in save-submission endpoint

--- backend/index.js
+++ backend/index.js
@@ -269,7 +269,8 @@ app.post("/api/pdfs/:pdfId/save-submission", upload.any(), async (req, res) => {
                 if (env === "development") {
                     values[key] = file.path;
                 } else {
-                    values[key] = await storage.uploadFile(`pdf-files/${pdfId}/${userEmail}/${Date.now()}/${file.originalname}`, file);
+                    let uploadData = await storage.uploadFile(`pdf-files/${pdfId}/${userEmail}/${Date.now()}/${file.originalname}`, file);
+                    values[key] = uploadData.url;
                 }
             }
         }
